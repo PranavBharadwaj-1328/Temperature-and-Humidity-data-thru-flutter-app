8d78b2296a2cb9b0e7ba83577af1b0fb6a5a854d
Pranav <bharadwaj.pranav1328@gmail.com>
2021-05-31 18:09:21 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/PB/tempdata/dhtdata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.PB.tempdata.dhtdata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
+      title: 'DHT Data',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage("0", "0", title: 'Weather Monitoring'),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MyHomePage extends StatefulWidget {
+  MyHomePage(this.temp, this.humid, {Key key, this.title}) : super(key: key);
+  final String title;
+  String temp;
+  String humid;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _getDht() async {
+    var url = Uri.parse("http://192.168.1.104:8080");
+    while (true) {
+      var result = await http.get(url);
+      setState(() {
+        widget.temp = result.body.split(",")[0];
+      });
+      print(widget.temp);
+      setState(() {
+        widget.humid = result.body.split(",")[1];
+      });
+      print(widget.humid);
+      sleep(Duration(seconds: 4));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDht();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            new Container(
+              child: SfRadialGauge(
+                axes: <RadialAxis>[
+                  RadialAxis(minimum: 0.0, maximum: 50.0, ranges: <GaugeRange>[
+                    GaugeRange(
+                        startValue: 0.0, endValue: 20.0, color: Colors.blue),
+                    GaugeRange(
+                        startValue: 20.0, endValue: 35.0, color: Colors.orange),
+                    GaugeRange(
+                        startValue: 35.0, endValue: 50.0, color: Colors.red)
+                  ], pointers: <GaugePointer>[
+                    NeedlePointer(value: double.parse(widget.temp))
+                  ], annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        widget: Container(
+                            child: Text("Temperature\n     "+widget.temp + "\u2103",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold))),
+                        angle: 90,
+                        positionFactor: 0.5)
+                  ])
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            new Container(
+              child: SfRadialGauge(
+                axes: <RadialAxis>[
+                  RadialAxis(minimum: 0.0, maximum: 100.0, ranges: <GaugeRange>[
+                    GaugeRange(
+                        startValue: 0.0, endValue: 50.0, color: Colors.blue),
+                    GaugeRange(
+                        startValue: 50.0, endValue: 75.0, color: Colors.orange),
+                    GaugeRange(
+                        startValue: 75.0, endValue: 100.0, color: Colors.red)
+                  ], pointers: <GaugePointer>[
+                    NeedlePointer(value: double.parse(widget.humid))
+                  ], annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        widget: Container(
+                            child: Text("Humidity\n  "+widget.humid + "%",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold))),
+                        angle: 90,
+                        positionFactor: 0.5)
+                  ])
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
